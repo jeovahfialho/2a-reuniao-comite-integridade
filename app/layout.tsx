08af78424fd5b8af3ad581a49f8d0973e34fcf78
Jeovah Fialho <jeovahfialho@gmail.com>
2025-10-16 22:00:59 -0300

    Update metadata title and description in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: '2a Reunião - Comite Integridade - Simples',
+  description: 'Created by Tawanna',
   generator: 'v0.app',
 }
 
